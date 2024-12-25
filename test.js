58bb2f1eeb42b1545c0890226257497ce2b7eef0
“Akash <akashkedariya007@gmail.com>
2024-12-25 15:34:42 +0530

    Second commit

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-console.log('====Hello========')
+
+const accountId = 12345678                   // Not change beacause 'const'
+let accountEmail = "samip@gmail.com"            // Current use
+var accountPassword = "samip@123"               // old time use
+accountCity = 'SUrat'                            // default
+
+// accountId = 55555555                     
+accountEmail = "alpesh@gmail.com"
+
+// console.log(accountId)
+// console.table([accountEmail, accountPassword, accountCity])
+
+let x ;
+// x ++ ;
+// x ++ ;
+// console.log(typeof x)
+
+let result = multiply(4,5)
+
+function multiply(p1,p2){
+
+    return p1 * p2 
+}
+
+// console.log(result)
+
+// ================================================================================
+
+// let stu_obj = {}
+// stu_obj.name = 'Hiren'
+// stu_obj.city = 'Surat'
+// stu_obj.name = 'Tarun'
+// console.log(stu_obj.name)
